56e1f9f74b655862af920e1f025c5e81252335f6
tomoda806 <tomoda.business@gmail.com>
2024-11-26 15:45:54 +0900

    add: blade追加・詳細表示機能実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -30,4 +30,12 @@ class TodoController extends Controller
 
         return redirect()->route('todo.index');
     }
+
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+        // dd($todo);
+        return view('todo.show', ['todo' => $todo]);
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -16,6 +16,6 @@ Route::get('/', function () {
 });
 
 Route::get('/todo', 'TodoController@index')->name('todo.index');
-Route::get('/todo/create', 'TodoController@create');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
